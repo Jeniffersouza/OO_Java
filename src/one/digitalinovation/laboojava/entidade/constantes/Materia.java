e066c8333054ff940016184c6895007e4c7fa1d9
Jeniffer souza <ajenifferbsouza@gmail.com>
2023-09-10 01:11:45 -0300

    Add files via upload

---
+++ src/one/digitalinovation/laboojava/entidade/constantes/Materia.java
@@ -0,0 +1,24 @@
+package one.digitalinovation.laboojava.entidade.constantes;
+
+public enum Materia {
+
+	 	M2(15),
+
+	    M5(10),
+
+	    M10(5);
+
+	    private double fator;
+
+	    /**
+	     * Construtor.
+	     * @param fator Valor por tipo que influencia no cálculo do frete.
+	     */
+	    Materia(double fator) {
+	        this.fator = fator / 100;
+	    }
+
+	    public double getFator() {
+	        return fator;
+	    }
+}
